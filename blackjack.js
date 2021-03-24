3fce1060b5b6961bd75c1da68794f724926cc456
Stormle <alvar.stara@gmail.com>
2021-03-24 04:19:13 +0200

    tick system debug + bugfixes

--- blackjack.js
+++ blackjack.js
@@ -2,11 +2,19 @@ const participants = require("./participants");
 const jackengine = require("./jackengine");
 
 
-var testparticipantarray = [];
-
 var jack = new participants.Player("jack", "123123123", "0", "100000");
 var bobby = new participants.Player("bobby", "789789", "0", "100000");
-testparticipantarray.push(jack);
-testparticipantarray.push(bobby);
-var table1 = new jackengine.Table(testparticipantarray);
 
+var table1 = new jackengine.Table();
+
+table1.addplayer = jack;
+table1.addplayer = bobby;
+
+//Override players to join the game (debug stuff)
+jack.originalbet = 10;
+bobby.originalbet = 33;
+table1._playersthisround.push(jack)
+table1._playersthisround.push(bobby)
+table1.tablestate = "round start"
+table1.tick;
+//Debug stuff ends
